1205e5f58572942990536cb043bdbbd33ee8db63
ArtMonts <monteirotecnicoti@gmail.com>
2024-11-12 23:16:24 -0300

    finalizei as questões 5 e 6

--- novembro/atividade6.c
+++ novembro/atividade6.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <limits.h>  // Para usar INT_MAX e INT_MIN
+void maiorNum(int num, int *maior, int *menor){
+     *maior = INT_MIN;
+    *menor = INT_MAX;
+    while(num >0){
+        //pega o ultimo digito
+        int digito = num%10;
+        
+        if(digito > *maior){
+            *maior = digito;
+        }
+
+        if(digito < *menor){
+            *menor = digito;
+        }
+        //tira o ultimo digito
+        num/=10;
+    }
+}
+
 int main(){
-    int num, resto;
+    int num, maior, menor;
+    //recebe o numero inteiro
     printf("Insira um numero inteiro: ");
     scanf("%d", &num);
-    while(num > 0 ){
-        resto = num%10;
-        printf("%d \n", resto );
-        num /=10;
-    }
+    //aplica na função
+    maiorNum(num, &maior, &menor);
+
+
+    printf("Maior algarismo: %d \n", maior);
+    printf("Menor algarismo: %d \n", menor);
+    return 0;
 }
